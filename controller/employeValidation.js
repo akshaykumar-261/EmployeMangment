9200571d036fafde49628a04bcfb7180a7ee18b8
Akshay Kumar~ <akshaykumar261@gmail.com>
2026-04-07 19:00:49 +0530

    third commit

--- controller/employeValidation.js
+++ controller/employeValidation.js
@@ -18,6 +18,18 @@ export const userIdValidator = Joi.object({
      email: Joi.string().email().required(),
      password: Joi.string().min(6).required(),
  });
+export const updateEmployeValidation = Joi.object({
+    id: Joi.string().required(),
+    name: Joi.string().max(100).required(),
+    lastname: Joi.string().max(100).optional().allow(null, ""),
+    email: Joi.string().email().required(),
+    phone: Joi.string().max(10).required(),
+    address: Joi.string().max(200).required(),
+    salary: Joi.number().optional(),
+    password: Joi.string().min(6).required(),
+    role: Joi.string().required(),
+    department: Joi.string().required(),
+ })
 export const validateRequest = (schema) => {
     return (req, res, next) => {
         const { error } = schema.validate(req.body);
